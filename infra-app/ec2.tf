17a2f8826ada0f99f60370300fcbcab84d16e769
Namratha <namratha343@gmail.com>
2025-05-20 03:18:22 +0530

    git

---
+++ infra-app/ec2.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "my_key_new" {
+    key_name = "${var.env}-infra-app-key"
+    public_key =  "" 
+}
--- infra-app/s3.tf
+++ infra-app/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "remote_s3" {
   bucket = "${var.env}-${var.bucket_name}"
 
   tags = {
-    Name        = "infra-app-bucket"
+    Name        = "${var.env}-${var.bucket_name}"
     Environment = var.env
   }
 
