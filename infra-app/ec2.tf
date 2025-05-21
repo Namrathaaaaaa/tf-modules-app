083430ebc5215368f7775a75b2b3e0aee3575926
Namratha <namratha343@gmail.com>
2025-05-21 23:09:55 +0530

    s3

--- infra-app/ec2.tf
+++ infra-app/ec2.tf
@@ -14,6 +14,14 @@ resource "aws_default_vpc" "default" {
 resource "aws_security_group" "my_security_group" {
   name        = "${var.env}-infra-app-sg"
   description = "This will add terraform integrated security group"
-  vpc_id        = aws_default_vpc.default.id
+  vpc_id      = aws_default_vpc.default.id
+
+}
+ingress [{
+    from_port: 22
+    to_port: 22
+    protocol: "tcp"
+    cidr_blocks = ["0.0.0.0/)0"]
+    description = "SSh open"
 
 }
