2e61f88a0fdca7d510767b734640be1dd8339447
Namratha <namratha343@gmail.com>
2025-05-22 21:21:09 +0530

    variables

--- infra-app/variables.tf
+++ infra-app/variables.tf
@@ -1,18 +1,15 @@
 variable "env" {
   description = "This is the environment"
   type        = string
-
 }
 
 variable "bucket_name" {
   description = "This is the name for my bucket"
   type        = string
-
 }
 
 
 variable "instance_count" {
   description = "This is the number of ec2 instance"
-  type = number
-  
+  type        = number
 }
